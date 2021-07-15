57bf7ed6c8eea092f086e0b4490f9240b11d3d95
thisdedd <thisdedd@gmail.com>
2021-07-15 16:32:21 -0300

    config redux

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
-export default function src() {
-  return <div>teste</div>;
+import Routes from "./routes";
+import Header from "./components/Header";
+
+import store from "./store";
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header />
+        <Routes />
+      </BrowserRouter>
+    </Provider>
+  );
 }
